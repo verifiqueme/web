ecd7d3063f03712fbeecfc43d23bc0c0c4dc7f84
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-24 00:09:22 -0300

    Arrumado css

--- src/App.vue
+++ src/App.vue
@@ -18,4 +18,24 @@ export default {
 </script>
 
 <style>
+html {
+  font-size: 14px;
+}
+@media (min-width: 768px) {
+  html {
+    font-size: 16px;
+  }
+}
+
+.container {
+  max-width: 960px;
+}
+
+.pricing-header {
+  max-width: 700px;
+}
+
+.card-deck .card {
+  min-width: 220px;
+}
 </style>
