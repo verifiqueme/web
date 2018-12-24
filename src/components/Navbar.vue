e8071460455fde0f111856d89ce7ab62f141ce8f
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-24 00:23:55 -0300

    Adicionado viewports corretos

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div
     class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm"
   >
-    <h5 class="my-0 mr-md-auto font-weight-normal">Verifica</h5>
+    <h5 class="my-0 mr-md-auto font-weight-normal">Ss</h5>
     <nav class="my-2 my-md-0 mr-md-3" v-for="item in items" :key="item.path">
       <router-link :to="item.path" active-class="active" class="p-2 text-dark" exact>{{ item.name }}</router-link>
     </nav>
@@ -12,8 +12,6 @@
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-vue/dist/bootstrap-vue.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
-import "bootstrap-vue/dist/bootstrap-vue.min.js";
 export default {
   name: "navbar",
   created() {
