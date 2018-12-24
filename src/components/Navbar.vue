6164798b5e40cb06fadc31d93d543f99015d2dc4
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-24 13:35:54 -0300

    Modificado estilos da navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <div class="container d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white">
     <h5 class="my-0 mr-md-auto font-weight-normal"></h5>
     <nav class="my-2 my-md-0 mr-md-3" v-for="item in items" :key="item.path">
-      <router-link :to="item.path" active-class="active" class="p-2 text-dark nav-link" exact>{{ item.name }}</router-link>
+      <router-link :to="item.path" active-class="active" class="p-2 nav-link" exact>{{ item.name }}</router-link>
     </nav>
   </div>
 </template>
@@ -27,7 +27,11 @@ export default {
 </script>
 
 <style>
+.nav-link{
+  color: #253031 !important;
+}
 .active {
-  color: blue !important;
+  color: #000 !important;
+  font-weight:bold;
 }
 </style>
