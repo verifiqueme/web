0aa3c2116305de7eac48e30b800aa52d0c7f796b
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-23 21:36:27 -0300

    [wip] Routes

---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello',
+    data () {
+      return {
+        msg: 'Olá mundo'
+      }
+    }
+  }
+</script>
