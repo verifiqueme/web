d5639f64b5fda3654f0230409892643816d7be74
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-23 23:41:28 -0300

    Adicionado bootstrap

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './routes'
 
-Vue.config.productionTip = false
+// Ext
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
