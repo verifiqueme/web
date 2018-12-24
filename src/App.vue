4bbf24e42555111d2a02002db3e80a90f4dd8cad
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-24 00:23:55 -0300

    Adicionado viewports corretos

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,6 @@
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
 import Navbar from "./components/Navbar";
 export default {
   name: "app",
@@ -18,24 +16,4 @@ export default {
 </script>
 
 <style>
-html {
-  font-size: 14px;
-}
-@media (min-width: 768px) {
-  html {
-    font-size: 16px;
-  }
-}
-
-.container {
-  max-width: 960px;
-}
-
-.pricing-header {
-  max-width: 700px;
-}
-
-.card-deck .card {
-  min-width: 220px;
-}
 </style>
