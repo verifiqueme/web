af634ab243b726ef16549da043d7ebbd8734cc63
Guilherme Scaranse <guiscaranse@gmail.com>
2018-12-24 00:05:17 -0300

    Navbar dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import Navbar from "./components/Navbar";
 export default {
-  name: 'app'
-}
+  name: "app",
+  components: {
+    Navbar
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
