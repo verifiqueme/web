8398d62ccc209ea80b4f55023631121be6902016
Guilherme Scaranse <guiscaranse@gmail.com>
2019-01-02 16:19:01 -0300

    Esqueci o _blank
    alzheimer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <div class="mastfoot mt-auto">
       <div class="inner">
         <p>Por <a href="https://twitter.com/guiscaranse" target="_blank">@guiscaranse</a>.</p>
-        <p>Veja este projeto no <a href="https://github.com/verificame"><font-awesome-icon :icon="['fab', 'github']"  /> Github</a>.</p>
+        <p>Veja este projeto no <a href="https://github.com/verificame" target="_blank"><font-awesome-icon :icon="['fab', 'github']"  /> Github</a>.</p>
       </div>
     </div>
 </template>
