fff119aa8a37065dfbebf4af9cdbdae5ac284bd0
Guilherme Scaranse <guiscaranse@gmail.com>
2019-01-03 16:11:16 -0300

    Adicionado extração dos dados da API

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -6,7 +6,15 @@
           <news-form @noticiaAnalise="analise"></news-form>
         </b-col>
       </b-row>
-      <score-card></score-card>
+      <b-row v-if="loading !== false">
+        <b-col>
+          <p>Analisando a notícia :)</p>
+          <b-progress :value="counter" variant="success" striped animated="true" class="mb-2"></b-progress>
+        </b-col>
+      </b-row>
+      <b-row  v-if="info">
+        <score-card :valor="info"></score-card>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -25,17 +33,31 @@
     },
     data() {
       return {
-        submited: true,
-        info: ""
+        submited: false,
+        loading: false,
+        info: "",
+        counter: 10
       };
     },
     methods: {
+      countUp(){
+        setTimeout(function() {
+          this.counter += 0.75;
+        }, 1000);
+      },
       analise(noticia) {
+        this.counter = 10;
+        this.countUp();
         let encoded = base64url.encode(noticia);
-        console.log(encoded);
+        this.submited = true;
+        this.loading = true;
         axios
           .get('https://verfcme-core.now.sh/api/' + encoded)
-          .then(response => (this.info = response.data))
+          .then(response => {
+            this.loading = false;
+            this.info = Math.round((response.data['response'] * 100));
+
+          })
           .catch(error => console.log(error))
       }
     }
