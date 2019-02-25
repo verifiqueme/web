3224efc41894424975b1341f724f04325c4e5efa
Guilherme Scaranse <guiscaranse@gmail.com>
2019-02-25 17:45:17 -0300

    Adicionado Hero do Bulma, e Navbar por Router

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,11 +1,40 @@
 <template>
-    
+  <div id="app">
+    <section class="hero is-bold is-fullheight">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <v-nav></v-nav>
+      </div>
+
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">Teste</h1>
+        </div>
+      </div>
+
+      <!-- Hero footer: will stick at the bottom -->
+      <div class="hero-foot">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              <strong>Olá</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Inicio"
+  import VNav from "./VNav";
+  export default {
+    name: "Inicio",
+    components: {
+      VNav
     }
+  }
 </script>
 
 <style scoped>
---
+++ src/components/VNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div :key="item.path" class="navbar-brand" v-for="item in items">
+        <router-link :to="item.path" active-class="active" class="navbar-item" exact>{{ item.name }}</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "v-nav",
+    created() {
+      this.$router.options.routes.forEach(route => {
+        this.items.push({
+          name: route.name,
+          path: route.path
+        });
+      });
+    },
+    data() {
+      return {
+        items: []
+      };
+    }
+  };
+</script>
