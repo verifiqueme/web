dcc2f2b558341ec7da54b2d6c8d6c84338b0eb37
Guilherme Scaranse <guiscaranse@gmail.com>
2019-02-26 00:24:31 -0300

    Criado design básico do formulário

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="app">
     <img class="logo" src="../assets/logo.svg" alt="">
-    <h1 class="title">Teste</h1>
+    <h1 class="title">Qual a probabilidade de uma notícia ser verdadeira?</h1>
+    <v-form></v-form>
   </div>
 </template>
 
 <script>
+  import VForm from "./VForm";
+
   export default {
     name: "Inicio",
+    components: {
+      VForm
+    }
   }
 </script>
 
 <style scoped>
-
-
   img.logo {
     padding-left: 20px;
+  }
+  h1.title{
+    font-size: 20px;
   }
 </style>
---
+++ src/components/VForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="columns is-mobile is-centered">
+    <div class="column is-half">
+      <div class="field has-addons has-addons-centered">
+        <b-tooltip :animated="true" label="Link da notícia" position="is-bottom" type="is-danger">
+          <div class="control has-icons-left">
+            <input :disabled="hasSubmit ?  '' : disabled" class="input is-large" type="url"
+                   v-model="url">
+            <span class="icon is-small is-left">
+            <i class="far fa-newspaper"></i>
+          </span>
+          </div>
+        </b-tooltip>
+        <div class="control">
+          <a :class="{'is-loading': hasSubmit}" :disabled="url ? disabled : ''" @click="enviar"
+             class="button is-danger is-large">
+            <i class="fas fa-search"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import isUrl from "is-url";
+
+  export default {
+    name: "v-form",
+    components: {},
+    data() {
+      return {
+        url: "",
+        hasSubmit: false
+      }
+    },
+    methods: {
+      enviar(){
+        this.hasSubmit = true;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
