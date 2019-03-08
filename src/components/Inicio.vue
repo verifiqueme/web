548d06d0ea078e3ff747085567f0abdc169cd508
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-08 13:25:17 -0300

    Colocado verificação de conexão

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="app">
-    <img class="logo" src="../assets/logo.svg" alt="">
+    <img alt="" class="logo" src="../assets/logo.svg">
     <h1 class="title">Qual a probabilidade de uma notícia ser verdadeira?</h1>
-    <v-form></v-form>
+    <b-notification :active="online !== 1" :closable="false" type="is-danger" has-icon icon-pack="fa">
+      Servidor está indisponível no momento.
+    </b-notification>
+    <v-form v-if="online === 1"></v-form>
+
   </div>
 </template>
 
 <script>
   import VForm from "./VForm";
+  import axios from "axios";
 
   export default {
     name: "Inicio",
     components: {
       VForm
+    },
+    data() {
+      return {
+        online: 1
+      }
+    },
+    watch: {
+      online: function () {
+        if (this.online === 1) {
+          this.$snackbar.open({
+            message: 'Conectado ao servidor.',
+            type: 'is-success',
+            position: 'is-top',
+            duration: 10000
+          })
+        }
+      }
+    },
+    created() {
+      setInterval(() => {
+        axios
+          .get('https://isitup.org/api.verifique.me.json')
+          .then(response => (
+            this.online = response.data.status_code
+          ))
+          .catch(error => this.online = 0)
+      }, 5000);
     }
   }
 </script>
 
 <style scoped>
   img.logo {
-    padding-left: 20px;
+    padding-left: 10px;
   }
-  h1.title{
+
+  h1.title {
     font-size: 20px;
   }
 </style>
