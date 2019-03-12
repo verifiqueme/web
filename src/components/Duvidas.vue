8cd6767e374664d0ee01f8640f105e75cf4a9519
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-12 18:59:55 -0300

    Adicionado página de dúvidas

---
+++ src/components/Duvidas.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="sobre">
+    <img alt="" class="logo" src="../assets/logo.svg">
+    <div class="columns is-mobile is-centered">
+      <div class="column">
+        <b-message :closable="false" size="is-large" title="Resultados e funcionamento" type="is-default">
+          Hoje a rede neural é avaliada em mais de 80% de precisão, com base em 359 notícias. Isto não significa que a
+          rede não pode errar. Ela tem constante dificuldade em avaliar textos recheados de opinião, é possível que
+          nestes casos a rede neural erre (seja avaliando o texto como verdadeiro ou falso). Por isto, o domínio das notícias, atualmente, é limitado em notícias de ampla repercussão nacional, falas de figuras públicas e entre outros.
+          <br>A rede neural está em constante atualização e melhorias que podem ser acompanhadas pelo repositório do Github.<br>
+          Um diagrama sobre o funcionamento do programa pode ser encontrado abaixo.
+        </b-message>
+        <img alt="" class="logo" src="../assets/fig_diagram.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Duvidas",
+  }
+</script>
+
+<style scoped>
+  img.logo {
+    padding-left: 10px;
+  }
+
+  h1.title {
+    font-size: 20px;
+  }
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from '../components/Inicio'
 import Sobre from "../components/Sobre";
+import Duvidas from "../components/Duvidas";
 
 Vue.use(Router);
 
@@ -9,6 +10,7 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'Inicio', component: Inicio },
-    { path: '/sobre', name: 'Sobre', component: Sobre }
+    { path: '/sobre', name: 'Sobre', component: Sobre },
+    { path: '/duvidas', name: 'Resultados e funcionamento', component: Duvidas }
   ]
 })
