0ff8ab56514a2d7b0f47643fc88e1cb73edf2c9c
Guilherme Scaranse <guiscaranse@gmail.com>
2019-03-12 18:50:55 -0300

    Adicionado "sobre"

---
+++ src/components/Sobre.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sobre">
+    <img alt="" class="logo" src="../assets/logo.svg">
+    <div class="columns is-mobile is-centered">
+      <div class="column">
+        <b-message :closable="false" size="is-large" title="Sobre" type="is-info">
+          O verifica.me foi originalmente criado por <a href="https://twitter.com/guiscaranse">@guiscaranse</a> como
+          parte de seu TCC <i>"Uso de Redes Neurais para detecção de notícias falsas na Internet"</i> no Instituto
+          Federal de Educação, Ciência e Tecnologia da Bahia.
+          É uma ferramenta de código aberto com mais de 80% de precisão, desenvolvida utilizando Python e Vue.js.<br>
+        </b-message>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Sobre",
+  }
+</script>
+
+<style scoped>
+  img.logo {
+    padding-left: 10px;
+  }
+
+  h1.title {
+    font-size: 20px;
+  }
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Inicio from '../components/Inicio'
+import Sobre from "../components/Sobre";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '',
-      name: 'Inicio',
-      component: Inicio
-    }
+    { path: '/', name: 'Inicio', component: Inicio },
+    { path: '/sobre', name: 'Sobre', component: Sobre }
   ]
 })
