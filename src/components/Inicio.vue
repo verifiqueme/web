1b401cb5b379e8630638b18326e3a205f3908db0
Guilherme Scaranse <guiscaranse@gmail.com>
2019-09-19 17:12:36 -0300

    Adicionado atualização

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -4,6 +4,13 @@
     <h1 class="title">Qual a probabilidade de uma notícia ser verdadeira?</h1>
     <div class="columns is-mobile is-centered">
       <div class="column is-half">
+        <b-message title="Autalização" type="is-success" aria-close-label="Fechar mensagem">
+          <strong>Novo servidor!</strong><br>Rede neural atualizada com mais de 600 noticias adicionadas ao banco de dados (totalizando 1100 no total).
+        </b-message>
+      </div>
+    </div>
+    <div class="columns is-mobile is-centered">
+      <div class="column is-half">
         <b-notification :active="online !== 1" :closable="false" type="is-danger">
           Servidor está indisponível no momento.
         </b-notification>
