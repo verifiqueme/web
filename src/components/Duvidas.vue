b264e94f8baa84bebf5e7bb05ab66bdf57ac93b1
Guilherme Scaranse <guiscaranse@gmail.com>
2019-09-19 12:50:11 -0300

    Atualizado números de notícias

--- src/components/Duvidas.vue
+++ src/components/Duvidas.vue
@@ -4,7 +4,7 @@
     <div class="columns is-mobile is-centered">
       <div class="column">
         <b-message :closable="false" size="is-large" title="Resultados e funcionamento" type="is-default">
-          Hoje a rede neural é avaliada em mais de 80% de precisão, com base em 359 notícias. Isto não significa que a
+          Hoje a rede neural é avaliada em mais de 80% de precisão, com base em <strong>1000 notícias</strong>. Isto não significa que a
           rede não pode errar. Ela tem constante dificuldade em avaliar textos recheados de opinião, é possível que
           nestes casos a rede neural erre (seja avaliando o texto como verdadeiro ou falso). Por isto, o domínio das notícias, atualmente, é limitado em notícias de ampla repercussão nacional, falas de figuras públicas e entre outros.
           <br>A rede neural está em constante atualização e melhorias que podem ser acompanhadas pelo repositório do Github.<br>
